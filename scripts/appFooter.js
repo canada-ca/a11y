11b9fa41930268441e334cab4458c5c266f73ba6
Shawn Thompson <plansmash@me.com>
2020-12-09 13:25:52 -0500

    add contribute links to footer

--- scripts/appFooter.js
+++ scripts/appFooter.js
@@ -1,3 +1,13 @@
 var defFooter = document.getElementById("def-footer");
 defFooter.outerHTML = wet.builder.appFooter({
+	"footerSections": [{
+		"href": "https://github.com/canada-ca/a11y/",
+		"text": "Contribute to this project"
+	}, {
+		"href": "https://github.com/canada-ca/a11y/issues/new?title=Problem%20with%20site%3A%0A",
+		"text": "Report issues on this project"
+	}, {
+		"href": "https://github.com/canada-ca/a11y/issues/new?title=Suggestion%3A%0A",
+		"text": "Suggest a change on this project"
+	}]
 });
